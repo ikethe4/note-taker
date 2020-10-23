2049d07de50df803911db2b651f71281e526550c
Isaac Arnold <isaac@iaivphotography.com>
2020-10-22 22:47:37 -0500

    last change

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.delete("/api/notes/:id", function (req, res) {
     res.json(arrBank);
 })
 
-app.get("/", function (req, res) {
+app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"))
 });
 
